787a9c857b5c303e3fdcabd55ff574da58dcb90e
surafel getahun <surafelgetahun01@gmail.com>
2022-11-12 14:06:11 +0300

    edge function added

--- graph.py
+++ graph.py
@@ -15,3 +15,15 @@ class Graph():
         self.nodes = []
         self.edges = {}
         self.adjacentNodes = {}
+    def add_edge(self, left, right,cost):
+        if left not in self.nodes:
+            self.nodes.append(left)
+            self.adjacentNodes[left] = set([])
+        if right not in self.nodes:
+            self.nodes.append(right)
+            self.adjacentNodes[right] = set([])
+        edge = Edge(left,right ,cost)
+        self.edges[(edge.left,edge.right)] = edge.cost
+        self.edges[(edge.right,edge.left)] = edge.cost
+        self.adjacentNodes[left].add(right)
+        self.adjacentNodes[right].add(left)
