1e258e7488e6e8569f726c47b9194021a2c9bb1a
surafel getahun <surafelgetahun01@gmail.com>
2022-11-13 09:51:58 +0300

    build graph structure

--- graph.py
+++ graph.py
@@ -27,3 +27,33 @@ class Graph():
         self.edges[(edge.right,edge.left)] = edge.cost
         self.adjacentNodes[left].add(right)
         self.adjacentNodes[right].add(left)
+# graph loader 
+def build_graph(file):
+    graph = Graph()
+    nodes = {}
+    file = open(file)
+    for line in file:
+        line =line.split(' ')  # this function performs to split one line text  with the spaces in to the list
+# creating nodes with given line list.
+        left = Node(line[0],float(line[1]),float(line[2])) 
+        right = Node(line[3],float(line[4]),float(line[5]))
+        weight = int(line[6])
+# The two consecutive if else statements are used to check  wheather left and right node is already created nodes in the nodes
+        if left.data not in nodes:
+            nodes[left.data]  = left
+        else:
+            left = nodes[left.data]
+        if right.data not in nodes:
+            nodes[right.data] = right
+        else:
+            right = nodes[right.data]
+        graph.add_edge(left,right,weight)
+
+##### you can check the loader by decomment the below code #######
+    for i in  graph.nodes:
+        print("the fontiers of ===> ",i.data)
+        for j in graph.adjacentNodes[i]:
+            print(j.data)
+
+
+build_graph("Orignal_twenty_node.txt")
